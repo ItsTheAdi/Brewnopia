9da67f0cc9c14fdc4d3f522919b4ded5c2778f4e
Aditya Pandey <91423208+ItsTheAdi@users.noreply.github.com>
2024-12-20 15:12:46 +0530

    Change image layouts

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,7 +7,7 @@ function nav() {
     <div className= 'h-13 m-0 font-serif text-xl text-grey-600 bg-orange-500 gap-5 flex justify-between items-center'>
         <div className='flex-row items-center justify-center'>
         <img className='items-center rounded-5 5 border-r-1' src={im} alt ={im}></img>
-        <h1 className='bg-black flex items-center justify-center text-orange-600'>Sip.Savour.Smile</h1>
+        <h1 className='bg-black flex items-center justify-center text-orange-600'>Sip.Okay.Smile</h1>
         </div>
         <div>
         <button className='p-3
